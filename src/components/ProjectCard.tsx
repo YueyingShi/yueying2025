fa5ffc1770bd397833564ede4b1dfae9762ed741
Ellan <sharonstone0524@gmail.com>
2025-08-07 19:11:40 +0200

    ESLint rules

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -14,23 +14,26 @@ type Project = {
 
 export function ProjectCard({ project }: { project: Project }) {
   return (
-    <div className=" bg-white p-6 rounded-xl overflow-clip flex flex-col gap-1 h-80 items-baseline shadow-lg hover:rotate-1 hover:scale-102 hover:shadow-xl transition-all">
-      <Image
-        src={project.icon}
-        alt={project.title}
-        className="h-32 max-w-48  mb-2  object-contain mix-blend-darken"
-      />
-      <div className="flex-1 flex flex-col gap-1">
-        {Array.isArray(project.details.Scope) &&
-          project.details.Scope.map((scope) => (
-            <Badge key={scope} text={scope} />
-          ))}{" "}
-      <div className="flex gap-2 mt-4">
-        {Array.isArray(project.details.Scope) &&
-          project.details.Scope.map((scope) => (
-            <Badge key={scope} text={scope} />
-          ))}{" "}
+    <>
+      <div className=" bg-white p-6 rounded-xl overflow-clip flex flex-col gap-1 h-80 items-baseline shadow-lg hover:rotate-1 hover:scale-102 hover:shadow-xl transition-all">
+        <Image
+          src={project.icon}
+          alt={project.title}
+          className="h-32 max-w-48  mb-2  object-contain mix-blend-darken"
+        />
+        <div className="flex-1 flex flex-col gap-1">
+          <h3 className="text-lg font-semibold text-gray-800">
+            {project.title}
+          </h3>
+          <p className="text-sm text-gray-600">{project.short_description}</p>
+        </div>
+        <div className="flex gap-2 mt-4">
+          {Array.isArray(project.details.Scope) &&
+            project.details.Scope.map((scope) => (
+              <Badge key={scope} text={scope} />
+            ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
