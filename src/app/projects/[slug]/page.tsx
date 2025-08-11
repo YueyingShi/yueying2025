23a5ba43553cb136fd588100b810307ea48c3def
Ellan <sharonstone0524@gmail.com>
2025-08-11 18:00:40 +0200

    ESLint rules

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -21,7 +21,11 @@ const projectComponents: ProjectComponentMap = {
   yiui: Yiui,
 };
 
-export default async function ProjectPage({ params }: ProjectPageProps) {
+export default async function ProjectPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const project = projects.find((p) => p.slug === params.slug);
   if (!project) return notFound();
 
