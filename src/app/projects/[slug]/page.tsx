d279537c6191a1b568ada87fa94fb989f1ef5ab3
Ellan <sharonstone0524@gmail.com>
2025-08-12 16:29:11 +0200

    last time, I am tired

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -19,7 +19,7 @@ const projectComponents: ProjectComponentMap = {
 export default async function ProjectPage({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }) {
   const { slug } = await params;
   const project = projects.find((p) => p.slug === slug);
